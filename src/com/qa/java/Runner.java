ad21386e835d378ccd8abe49a65ac1012fa7304d
M-java86 <mpythondata@outlook.com>
2021-05-18 00:21:34 -0400

    MO: added Solution for Task -- needs to be refactored-unfinsihed.

--- src/com/qa/java/Runner.java
+++ src/com/qa/java/Runner.java
@@ -1,8 +1,61 @@
 package com.qa.java;
 
-public class Runner {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+//public class Runner {
+	//public static void main(String[] args) {
+	//	System.out.println("Hello World!");
+	//}
 
-}
+//}
+
+
+
+
+
+
+
+
+//Flowchart exercise
+
+
+//Java
+
+
+
+
+//public static void flowchart(int a) {​
+//if (a > 2000) {​
+   // System.out.println("A");
+   // if (a > 6000) {​
+    //    System.out.println("C");
+        // THE END            }​ else {​
+       // System.out.println("B");
+       // if (a > 4000) {​
+          //  System.out.println("D");
+         //   // THE END                }​ else {​
+           // System.out.println("E");
+            // THE END                }​
+  //  }​
+//}​ else {​
+   // System.out.println("1");
+//   if (a > 100) {​
+ //       System.out.println("3");
+   //     if (a > 600) {​
+          //  System.out.println("5");
+     ///       // THE END                }​ else {​
+        ///   System.out.println("4");
+/          //  if (a > 500) {​
+               // System.out.println("6");
+ /               // THE END                    }​ else {​
+  //              System.out.println("7");
+    /            // THE END                    }​
+      //  }​
+   // }​ else {​
+      //  System.out.println("2");
+        // THE END            }​
+//}​
+//}​
+
+
+
+
+//(4 liked)<https://teams.microsoft.com/l/message/19:c4718643586c4b0dbf9ff4efdfa27d35@thread.tacv2/1621276427508?tenantId=9c220797-c337-49f8-b086-a9a24979f728&amp;groupId=18567f64-67ce-4ef2-9eef-1cfed80bb05e&amp;parentMessageId=1621260056060&amp;teamName=QA-Infosys Bootcamp-Core&amp;channelName=The Main Room - Lectures, Demos&amp;createdTime=1621276427508>
